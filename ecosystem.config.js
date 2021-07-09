155e2ace92d65dbd138f898dcae551690685f64a
David Sandoz <david@sandoz.pro>
2021-07-09 21:37:59 +0200

    ci: specify deploy steps in CI instead of PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,17 +7,7 @@ module.exports = {
       watch: false,
       node_args: '',
       merge_logs: true,
+      cwd: process.env.HOME + '/sites/tchoukball.ch',
     },
   ],
-  deploy: {
-    production: {
-      ref: 'origin/main',
-      repo: 'https://github.com/SwissTchoukball/website-next',
-      path: process.env.HOME + '/sites/tchoukball.ch',
-      'post-deploy': 'npm ci && npm run build && pm2 restart ecosystem.config.js --env production',
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
-  },
 };
