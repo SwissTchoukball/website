5e493e35e283d54007cd1678bf624e92ba4885c3
David Sandoz <david@sandoz.pro>
2021-08-04 12:40:13 +0200

    feat(core): add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,14 @@ const config: NuxtConfig = {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'apple-touch-icon', sizes:'180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes:'32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes:'16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ff0000' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
