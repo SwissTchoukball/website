1d4fd571fc085f0690dc421f40a8086cdcbaba78
David Sandoz <david@sandoz.pro>
2021-12-27 10:14:57 +0100

    fix(core): create impressum and medias pages
    They are simple pages provided by the CMS,
    and work simply with _.vue.
    But to link to them through localePath,
    we need them to exist in the pages folder

---
+++ pages/impressum.vue
@@ -0,0 +1,12 @@
+<template>
+  <st-simple-page :title="title" :body="body" :key-roles="keyRoles" :resources="resources" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import CatchAllPage from '~/pages/_.vue';
+
+export default Vue.extend({
+  extends: CatchAllPage,
+});
+</script>
