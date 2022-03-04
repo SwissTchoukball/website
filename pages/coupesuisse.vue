8b0422512232c8aa721496387a4de74764802192
David Sandoz <david@sandoz.pro>
2022-03-04 16:53:12 +0100

    feat(coupesuisse): add redirect from /coupesuisse

---
+++ pages/coupesuisse.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  asyncData({ app, redirect }) {
+    redirect(
+      app.localePath({
+        name: 'competitions-competition',
+        params: {
+          competition: 'coupe-suisse',
+        },
+      })
+    );
+  },
+});
+</script>
