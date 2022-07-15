454eb32f32a158217d2f92b015bf30430fcb4495
David Sandoz <david@sandoz.pro>
2022-07-15 16:41:56 +0200

    feat(competitions): add redirect for swiss cup and championship

--- pages/championship.vue
+++ pages/championship.vue
@@ -1,12 +1,18 @@
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
+  nuxtI18n: {
+    paths: {
+      fr: '/championnat',
+      de: '/meisterschaft',
+    },
+  },
   asyncData({ app, redirect }) {
     redirect(
       app.localePath({
         name: 'competitions-competition',
         params: {
-          competition: 'coupe-suisse',
+          competition: app.i18n.locale === 'de' ? 'liga-a' : 'ligue-a',
         },
       })
     );
