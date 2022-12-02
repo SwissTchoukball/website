7401bb640f6ba0fe0e3b8248004dce6ee67d4c4e
David Sandoz <david@sandoz.pro>
2022-12-02 16:02:54 +0100

    fix(calendar): add redirections from year and month index

---
+++ pages/calendar/_year/_month/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  nuxtI18n: {
+    paths: {
+      fr: '/calendrier/:year/:month',
+      de: '/kalender/:year/:month',
+    },
+  },
+  asyncData({ redirect, localePath, route }) {
+    redirect(
+      localePath({
+        name: 'calendar-year-month-list',
+        params: { year: route.params.year, month: route.params.month },
+      })
+    );
+  },
+});
+</script>
---
+++ pages/calendar/_year/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  nuxtI18n: {
+    paths: {
+      fr: '/calendrier/:year',
+      de: '/kalender/:year',
+    },
+  },
+  asyncData({ redirect, localePath, route }) {
+    redirect(
+      localePath({
+        name: 'calendar-year-month-list',
+        params: { year: route.params.year, month: '1' },
+      })
+    );
+  },
+});
+</script>
