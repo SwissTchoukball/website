68f419871d81be423bd960098491e3618116f451
David Sandoz <david.sandoz@tchoukball.ch>
2023-05-27 19:21:22 +0200

    fix(national teams): add logo to WYTC 2023 pages

--- pages/wytc2023.vue
+++ pages/wytc2023.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="l-main-content-section">
+    <img v-if="competition" :src="logoSrc" :srcset="logoSrcSet" class="competition-logo" />
     <h2 class="t-headline-1">{{ $t('internationalCompetition.wytc2023.title') }}</h2>
     <st-navigation
       :items="navigation"
@@ -14,11 +15,14 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { NationalTeamCompetition } from '~/components/national-teams/st-national-teams.prop';
+import { getAssetURL } from '~/plugins/directus';
 import { MenuItem } from '~/store/state';
 
 export default Vue.extend({
   data() {
     return {
+      competition: undefined as NationalTeamCompetition | undefined,
       navigation: [
         { name: this.$t('internationalCompetition.navigation.about'), href: this.localePath('wytc2023-about') },
         { name: this.$t('internationalCompetition.navigation.schedule'), href: this.localePath('wytc2023-schedule') },
@@ -27,12 +31,42 @@ export default Vue.extend({
       ] as MenuItem[],
     };
   },
+  async fetch() {
+    this.competition = await this.$cmsService.getNationalTeamCompetition(25);
+  },
+  computed: {
+    logoSrc(): string {
+      if (!this.competition) {
+        return '';
+      }
+      return getAssetURL(this.$config.cmsURL, this.competition.logo, { width: 180 });
+    },
+    logoSrcSet(): string {
+      if (!this.competition) {
+        return '';
+      }
+      return `${getAssetURL(this.$config.cmsURL, this.competition.logo, {
+        width: 360,
+      })} 2x`;
+    },
+  },
 });
 </script>
 
 <style scoped>
+.competition-logo {
+  margin: auto;
+  display: block;
+}
+
 .navigation {
   margin-top: var(--st-length-spacing-s);
   margin-bottom: var(--st-length-spacing-s);
+}
+
+@media (--sm-and-up) {
+  .competition-logo {
+    float: right;
+  }
 }
 </style>
