4cc0fb02bc3e91f6bd3d23ca6466c8cdec8f685f
David Sandoz <david.sandoz@tchoukball.ch>
2023-05-27 10:57:06 +0200

    fix(etc2022): replace twitter timeline with link

--- pages/etc2022/index.vue
+++ pages/etc2022/index.vue
@@ -1,30 +1,24 @@
 <template>
-  <div>
+  <div class="c-etc2022">
     <p v-if="$fetchState.error">{{ $t('error.otherError') }} : {{ $fetchState.error.message }}</p>
     <!-- We have to use v-html here as we get html content directly from Directus -->
     <!-- eslint-disable-next-line vue/no-v-html -->
     <div v-else-if="!$fetchState.pending" class="directus-formatted-content" v-html="textEntry.body"></div>
-    <a
-      class="twitter-timeline"
-      :data-lang="$i18n.locale"
-      data-dnt="true"
-      data-width="800"
-      data-link-color="#ff0000"
-      href="https://twitter.com/SwissTchoukball?ref_src=twsrc%5Etfw"
+    <st-button
+      href="https://twitter.com/search?q=%23etc2022UK%20(from%3ASwissTchoukball)&f=live"
+      class="c-etc2022__tweets-button"
+      primary
     >
-      <st-loader />
-    </a>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      {{ $t('etc2022.viewTweets') }}
+    </st-button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import stLoader from '~/components/st-loader.vue';
 import { TextEntry } from '~/plugins/cms-service';
 
 export default Vue.extend({
-  components: { stLoader },
   data() {
     return {
       textEntry: undefined as TextEntry | undefined,
@@ -37,10 +31,10 @@ export default Vue.extend({
 </script>
 
 <style>
-.twitter-timeline {
-  /* !important is used to override Twitter iframe */
-  display: block !important;
-  margin: auto;
-  margin-top: var(--st-length-spacing-s) !important;
+.c-etc2022 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: var(--st-length-spacing-s);
 }
 </style>
