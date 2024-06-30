7383cd3d5e039e5638a650ec42c3017c91094a5d
David Sandoz <david.sandoz@tchoukball.ch>
2024-06-30 16:54:05 +0200

    chore(pm2): fix ecosystem config file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,6 @@ module.exports = {
   apps: [
     {
       name: process.env.PM2_APP_NAME,
-      port: 3000,
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
