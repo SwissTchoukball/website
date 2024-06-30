255fae1c20929e4145a391aab3880b96a343ad71
David Sandoz <david.sandoz@tchoukball.ch>
2024-06-30 15:40:25 +0200

    chore: remove args from pm2 ecosystem config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: process.env.PM2_APP_NAME,
+      port: 3000,
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
-      args: 'start',
       merge_logs: true,
       cwd: process.env.PWD,
     },
