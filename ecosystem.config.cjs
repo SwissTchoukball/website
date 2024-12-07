8fc49e35b8a174c085b03846743f4cc00685de73
David Sandoz <david.sandoz@tchoukball.ch>
2024-12-07 20:21:03 +0100

    chore(pm2): set amount of instances from env variable

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: process.env.PM2_APP_NAME,
       exec_mode: 'cluster',
-      instances: 'max',
+      instances: process.env.PM2_AMOUNT_INSTANCES || 1,
       script: './.output/server/index.mjs',
       merge_logs: true,
       cwd: process.env.PWD,
