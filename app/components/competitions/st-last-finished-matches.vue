2e7b94733a5a94a756a0686e73a0bed4355707ea
David Sandoz <david.sandoz@tchoukball.ch>
2026-06-19 09:50:34 +0200

    feat(matches): placing the forfeited matches at end of the list of finished matches

--- app/components/competitions/st-last-finished-matches.vue
+++ app/components/competitions/st-last-finished-matches.vue
@@ -49,8 +49,21 @@ const { matchesData, competitionEditions, getPathToEditionSchedule, fetchPending
 const areMoreResultsVisible = ref(false);
 const matchesToShow = computed(() => (areMoreResultsVisible.value ? matchesData.value.length : 3));
 
+const sortedMatchesData = computed(() => {
+  // Placing the forfeited matches at the end of the list
+  return matchesData.value.toSorted((a, b) => {
+    if (a.match.home_team_score === 0 || a.match.away_team_score === 0) {
+      return 1;
+    }
+    if (b.match.home_team_score === 0 || b.match.away_team_score === 0) {
+      return -1;
+    }
+    return 0;
+  });
+});
+
 const visibleMatchesData = computed(() => {
-  return matchesData.value.slice(0, matchesToShow.value);
+  return sortedMatchesData.value.slice(0, matchesToShow.value);
 });
 </script>
 
