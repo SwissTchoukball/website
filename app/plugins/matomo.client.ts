1760330e17dae28e72860a97a430e8c1eb4d22e8
David Sandoz <david.sandoz@tchoukball.ch>
2026-08-03 13:35:03 +0200

    fix(matomo): handle CJS default export interop for vue-matomo with Vite 8

--- app/plugins/matomo.client.ts
+++ app/plugins/matomo.client.ts
@@ -1,10 +1,20 @@
 import { defineNuxtPlugin } from '#app';
-import VueMatomo from 'vue-matomo';
+import _VueMatomo from 'vue-matomo';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const runtimeConfig = useRuntimeConfig();
 
-  nuxtApp.vueApp.use(VueMatomo, {
+  // vue-matomo is a CJS/UMD bundle. In some bundler configurations
+  // (observed with Vite 8 / Nuxt 4.5+) the default export is wrapped
+  // in a namespace object. We unwrap it here to guarantee we pass the
+  // actual install function to Vue.
+  const VueMatomoPlugin =
+    typeof _VueMatomo === 'function'
+      ? _VueMatomo
+      : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (_VueMatomo as any).default || _VueMatomo;
+
+  nuxtApp.vueApp.use(VueMatomoPlugin, {
     host: runtimeConfig.public.matomo.url,
     siteId: runtimeConfig.public.matomo.siteId,
     // Enables automatically registering pageviews on the router
